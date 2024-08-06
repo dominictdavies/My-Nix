d157b38af9c72ddb26497d77fdf811671b5424b4
Dominic Davies <dominictdavies@gmail.com>
2024-08-07 02:38:53 +0800

    Bluetooth and more packages

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,9 @@
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  # Enable bluetooth
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
@@ -98,8 +101,11 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    alacritty
+    git
+    gh
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
