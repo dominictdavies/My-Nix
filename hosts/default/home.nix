384c77d63a0c690729fef46f986f18dcd3573523
Dominic Davies <dominictdavies@gmail.com>
2024-10-07 15:51:58 +0800

    Remove alacritty

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -22,8 +22,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # Terminal
-    alacritty
+    # Terminal helpers
     fzf
     zoxide
 
