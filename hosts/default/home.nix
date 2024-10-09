ecd09d359fe2f4bb1a5fe0af4342cf2adf5e6f44
Dominic Davies <dominictdavies@gmail.com>
2024-10-09 19:39:58 +0800

    Revert "Remove alacritty"
    This reverts commit 384c77d63a0c690729fef46f986f18dcd3573523.

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -22,7 +22,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    # Terminal helpers
+    # Terminal
+    alacritty
     fzf
     zoxide
 
